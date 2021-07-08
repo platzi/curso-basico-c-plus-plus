7bd2bf532907ba269109c55e753aba169ef04659
edevars <e.devars.dev@gmail.com>
2021-07-08 10:51:39 -0500

    Operadores

---
+++ operadores/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a = 2;
+    int b = 3;
+    int edades[] = { 24,20, 7 };
+    cout << sizeof(edades) / sizeof(edades[0]);
+}
