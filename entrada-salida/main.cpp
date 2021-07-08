13fc660f8386ece795054b8f4970ccfc48262c85
edevars <e.devars.dev@gmail.com>
2021-07-08 10:54:47 -0500

    Entrada y salida de datos

---
+++ entrada-salida/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // esto es un comentario
+    /* 
+        esto es un comentario
+    */
+    int edad = 0;
+    cout << edad;
+    cin >> edad;
+    cout << "tu edad es: " << edad;
+}
